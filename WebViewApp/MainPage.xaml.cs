c7e434859ec81ea73e3fc798fa5cbf4d9ccae45b
Ahmed Hamdi Radwan <ahmed.radhwan@gmail.com>
2018-09-19 18:32:41 +0200

    Initial check-in of module WebViewApp

---
+++ WebViewApp/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WebViewApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void BackButton_Clicked(object sender, System.EventArgs e)
+        {
+            if(webView.CanGoBack)
+            {
+                webView.GoBack();
+            }
+        }
+
+        void ForwardButton_Clicked(object sender, System.EventArgs e)
+        {
+            if(webView.CanGoForward)
+            {
+                webView.GoForward();
+            }
+        }
+
+        //void RefreshButton_Clicked(object sender, System.EventArgs e)
+        //{
+        //    webView.Source = webView.Source;
+        //}
+
+        void Handle_Navigated(object sender, Xamarin.Forms.WebNavigatedEventArgs e)
+        {
+            loadingIndicator.IsVisible = false;
+            loadingIndicator.IsRunning = false;
+        }
+
+        void Handle_Navigating(object sender, Xamarin.Forms.WebNavigatingEventArgs e)
+        {
+            loadingIndicator.IsVisible = true;
+            loadingIndicator.IsRunning = true;
+        }
+    }
+}
