42c7f6d4cddbe52e22650bbcb266cdd7eeace057
Ahmed Hamdi Radwan <ahmed.radhwan@gmail.com>
2018-09-24 12:07:42 +0200

    Removed redundant resources

--- WebViewApp.Android/Resources/Resource.Designer.cs
+++ WebViewApp.Android/Resources/Resource.Designer.cs
@@ -3950,31 +3950,13 @@ namespace WebViewApp.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int baseline_arrow_back_24 = 2130903040;
+			public const int icon = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int baseline_arrow_back_black_24 = 2130903041;
+			public const int icon_round = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int baseline_arrow_forward_24 = 2130903042;
-			
-			// aapt resource value: 0x7f030003
-			public const int baseline_arrow_forward_black_24 = 2130903043;
-			
-			// aapt resource value: 0x7f030004
-			public const int baseline_refresh_24 = 2130903044;
-			
-			// aapt resource value: 0x7f030005
-			public const int baseline_refresh_black_24 = 2130903045;
-			
-			// aapt resource value: 0x7f030006
-			public const int icon = 2130903046;
-			
-			// aapt resource value: 0x7f030007
-			public const int icon_round = 2130903047;
-			
-			// aapt resource value: 0x7f030008
-			public const int launcher_foreground = 2130903048;
+			public const int launcher_foreground = 2130903042;
 			
 			static Mipmap()
 			{
